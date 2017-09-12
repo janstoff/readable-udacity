055428a1557a85058f05ddd4f19c9c085a0dadd9
Jan Steinhoff <jan.steinhoff@gmail.com>
2017-09-12 15:33:19 +0200

    fix posted time displayed

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,12 +1,12 @@
 
 export function timeConverter(timestamp){
-  var a = new Date(timestamp * 1000);
+  var a = new Date(timestamp);
   var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
   var year = a.getFullYear();
   var month = months[a.getMonth()];
   var date = a.getDate();
   var hour = a.getHours();
-  var min = a.getMinutes();
+  var min = a.getMinutes() < 10 ? '0' + a.getMinutes() : a.getMinutes();
   var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min;
   return time;
 }
